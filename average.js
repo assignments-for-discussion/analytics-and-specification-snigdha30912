4eb7a806cb7235661cdd7957f1cdb0f1753aafd4
sniggo jain <snigdha.jain30@gmail.com>
2021-12-19 15:52:43 +0530

    changed the code in average.js so that the function ignores NaN values if present and returns the average of the filtered values

--- average.js
+++ average.js
@@ -1,6 +1,9 @@
-
 function average(numbers) {
-  return numbers.reduce((p, c)=> p + c, 0) / numbers.length;
-}
+  // filtered array is created which ignores the NaN values and stores the rest of the values
+  const filteredArray = numbers.filter(function (value) {
+    return !Number.isNaN(value);
+})
+  return filteredArray.reduce((p, c)=> p + c, 0) / (filteredArray.length);
+ }
 
 module.exports = {average};
